1ab522f02c70ac0c00df4b6939d5a8d40f2030b1
Sal-DS <matheussalda2108@gmail.com>
2025-11-01 05:31:58 -0300

    Alterações leves no index e script com o intúito de aumentar a responsividade

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-const Poketypes =
-[
-  "normal",
-  "fire",
-  "water",
-  "grass",
-  "electric",
-  "ice",
-  "fighting",
-  "poison",
-  "ground",
-  "flying",
-  "psychic",
-  "bug",
-  "rock",
-  "ghost",
-  "dragon",
-  "steel",
-  "dark",
-  "fairy"
-]
+const Poketypes = [
+  ["normal", "fire", "water", "grass", "electric", "ice", "fighting", "poison", "ground", "flying", "psychic", "bug", "rock", "ghost", "dragon", "steel", "dark", "fairy"],
+  ["gray", "red", "blue", "green", "yellow", "lightblue", "brown", "purple", "orange", "lightgray", "pink", "lime", "tan", "indigo", "navy", "silver", "black", "lightpink"]
+];
 
 let rate = 0;
 let teamtypes = new Set();
@@ -27,15 +10,20 @@ const img = document.getElementsByClassName('pokeimg')[0];
 const pokename = document.getElementsByClassName('pokename')[0];
 const types = [document.getElementById('typeone'), document.getElementById('typetwo')];
 const ratetext = document.getElementsByClassName('powerRate')[0];
+const background = document.querySelector('.container');
 
 let pokeindex = 0;
 
+async function getPok(id) {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    let pokeinfo = await response.json();
+    return pokeinfo;
+}
+
 function randPok() {
   const id = Math.floor(Math.random() * 1050) + 1;
+  getPok(id).then(pokeinfo => {
   
-  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then(response => response.json())
-    .then(pokeinfo => {
         if(pokeindex < 6){
             pokename.innerHTML = pokeinfo.name;
             img.src = pokeinfo.sprites.front_default;
@@ -48,6 +36,7 @@ function randPok() {
                 teamtypes.add(pokeinfo.types[1].type.name);
             }
             ratetext.innerHTML = powerRate();
+            background.style.backgroundColor = Poketypes[1][Poketypes[0].indexOf(pokeinfo.types[0].type.name)];
         }
         else{
             alert("Você já tem um time completo!")
@@ -77,13 +66,13 @@ function addTeam(img, pokename){
 
 function powerRate(){
     if(pokeindex >= 6){
-        for (let index = 0; index < Poketypes.length; index++) {
-            if(teamtypes.has(Poketypes[index])){
+        for (let index = 0; index < 18; index++) {
+            if(teamtypes.has(Poketypes[0][index])){
                 rate++;
             }
         }
         let percent = ((rate * 100) / 12);
-        return "Poder da equipe: " + percent.toFixed(2) + "%";
+        return "Poder da equipe: " + percent.toFixed(0) + "%";
     }
     else{
         return null;
